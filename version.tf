1efc624fcd36ab1ccf0447a9a04b9aeba8cdbb5d
Evan Chime <evanchime@yahoo.co.uk>
2026-01-30 05:24:26 +0000

    chore: Add version.tf for provider and Terraform version constraints
    This commit introduces a `version.tf` file to centralize the required versions for Terraform and its providers (AWS and Kubernetes).
    
    This improves project structure by defining all version constraints in a single, dedicated file, making them easier to manage and update.

---
+++ version.tf
@@ -0,0 +1,19 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0.1"
+    }
+
+  }
+
+  required_version = "~> 1.3"
+}
